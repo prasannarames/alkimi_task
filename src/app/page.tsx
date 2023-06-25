d6b1ef35a6b48b553597d8578a7796c17a16f25a
prasannarames <prasannaramesh98@gmail.com>
2023-06-25 10:06:03 +0530

    update: assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import Image from 'next/image'
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen">
       <h1 className='text-white'>alkimi task</h1>
     </main>
   )
---
+++ src/assets/index.tsx
@@ -0,0 +1,41 @@
+import aboutImg from "./about-img.png";
+import barChartIcon from "./bar-chart.svg";
+import bgHoverImg from "./bg-hover.png";
+import cpuChipIcon from "./cpu-chip.svg";
+import cubeIcon from "./cpu-chip.svg";
+import dataflowIcon from "./dataflow.svg";
+import logo from "./logo.png";
+import menuClose from "./menu-close.png";
+import menuOpen from "./menu-open.svg";
+import minusIcon from "./minus.svg";
+import percentIcon from "./percent.svg";
+import person1 from "./person1.png";
+import person2 from "./person2.png";
+import person3 from "./person3.png";
+import plusIcon from "./plus.svg";
+import profileIcon from "./profile-icon.svg";
+import serverIcon from "./server.svg";
+import sheildIcon from "./shield.svg";
+import targetIcon from "./target.svg";
+
+export {
+  aboutImg,
+  barChartIcon,
+  bgHoverImg,
+  cpuChipIcon,
+  cubeIcon,
+  dataflowIcon,
+  logo,
+  menuClose,
+  menuOpen,
+  minusIcon,
+  percentIcon,
+  person1,
+  person2,
+  person3,
+  plusIcon,
+  profileIcon,
+  serverIcon,
+  sheildIcon,
+  targetIcon,
+};
